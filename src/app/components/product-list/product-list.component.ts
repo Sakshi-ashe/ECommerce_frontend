e88512fbd6df79478d92f0f7c01095aa93fb8c8e
Sakshi Gupta <sakshi.gupta3@globallogic.com>
2021-05-04 18:00:21 +0530

    product by id, keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,30 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
+import { ProductCategory } from 'src/app/common/product-category';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private productService : ProductService) { }
+  currentCategoryId: number;
+
+  currentCategoryName: string;
+
+  searchMode: boolean;
+
+  constructor(private productService : ProductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
 
   listProducts(){
-    this.productService.getProductsList().subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    console.log("########");
+    console.log(this.searchMode);
+    
+
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }
+    else{
+      this.handleListProducts();
+    }
   }
 
+ handleListProducts(){
+   //chack if "id " parameter is available.
+   const hasCategoryId : boolean = this.route.snapshot.paramMap.has('id');
+   console.log(hasCategoryId);
 
+   if(hasCategoryId){
+     // get the 'id' param string, convert string to a number using the  "+" symbol
+     this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+     console.log(this.currentCategoryId);
+
+     this.currentCategoryName = this.route.snapshot.paramMap.get('name');
+
+   }
+   else{
+     // not category id available.... default to category id 1
+     this.currentCategoryId = 1;
+     console.log(this.currentCategoryId);
+
+     this.currentCategoryName = 'Books';
+
+   }
+
+   //now get the products for this given category id
+   this.productService.getProductsList(this.currentCategoryId).subscribe(
+     data => {
+       this.products = data;
+     }
+   )
+
+ }
+
+ handleSearchProducts(){
+   const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+   //now search for the products using keyword
+   this.productService.searchProducts(theKeyword).subscribe(
+     data => {
+       this.products = data;
+     }
+   )
+ }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators'
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,46 @@ export class ProductService {
   // Note: initially this whole class is empty.
   // Below code is written by hand.
 
-  private baseUrl ='http://localhost:1717/api/products';
+  //private baseUrl ='http://localhost:1717/api/products';
+  //by default , only 20 items diplayed even when 100 items present .
   
+  private baseUrl ='http://localhost:1717/api/products';
+  private categoryUrl ='http://localhost:1717/api/product-category';
+
+
   constructor(private httpClient : HttpClient) { }
 
-  getProductsList() : Observable<Product[]>
+  getProductsList(theCategoryId : number) : Observable<Product[]>
   {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-            map(response => response._embedded.products)
-        )
+    //need to build url based on category id
+    const searchUrl =`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    console.log(searchUrl);
+
+    return this.getProducts(searchUrl);
+  }
+
+  getProductCategories(): Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+
+  }
+
+
+  searchProducts(theKeyword : string) : Observable<Product[]>{
+        //need to build url based on keyword
+        const searchUrl =`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+        console.log(searchUrl);
+  
+        return this.getProducts(searchUrl);
+    
+  }
+
+//select the repetitive code -> refactor -> to a method
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
   }
 
 }
@@ -27,12 +59,11 @@ export class ProductService {
 
 
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded:{
     products: Product[]
   }
 }
-
 
 /* see below json data fetched from http://localhost:1717/api/products 
 {
@@ -51,3 +82,33 @@ interface GetResponse{
         "self" : {
       
 */
+
+
+interface GetResponseProductCategory{
+  _embedded:{
+    productCategory: ProductCategory[];
+  }
+}
+
+
+// http://localhost:1717/api/product-category
+// {
+//   "_embedded" : {
+//     "productCategory" : [ {
+//       "id" : 1,
+//       "categoryName" : "Books",
+//       "_links" : {
+//         "self" : {
+//           "href" : "http://localhost:1717/api/product-category/1"
+//         },
+//         "productCategory" : {
+//           "href" : "http://localhost:1717/api/product-category/1"
+//         },
+//         "products" : {
+//           "href" : "http://localhost:1717/api/product-category/1/products"
+//         }
+//       }
+//     }, {
+//       "id" : 2,
+//       "categoryName" : "Coffee Mugs",
+//       "_links" 
